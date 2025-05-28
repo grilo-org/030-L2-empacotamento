30cd99e4515f2dfa3fa686b48cfab0e923a7fa05
wesleymrosa <wesleymrosa@gmail.com>
2025-05-28 19:14:39 -0300

    Incluída ProdutoDTO no pacote dto. Estrutura de dados com dimensões do produto para uso no empacotador

---
+++ src/main/java/br/wesley/empacotamentoapi/dto/ProdutoDTO.java
@@ -0,0 +1,40 @@
+package br.wesley.empacotamentoapi.dto;
+
+public class ProdutoDTO {
+    private String nome;
+    private int altura;
+    private int largura;
+    private int comprimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public void setLargura(int largura) {
+        this.largura = largura;
+    }
+
+    public int getComprimento() {
+        return comprimento;
+    }
+
+    public void setComprimento(int comprimento) {
+        this.comprimento = comprimento;
+    }
+}
