ab94115fdae148c3f7f77d5a85ef6cd260f8525c
wesleymrosa <wesleymrosa@gmail.com>
2025-05-28 20:05:04 -0300

    Incluída SecurityConfig no pacote config. Configuração de autenticação básica com usuário em memória e liberação do Swagger

---
+++ src/main/java/br/wesley/empacotamentoapi/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+// src/main/java/br/wesley/empacotamentoapi/config/SecurityConfig.java
+package br.wesley.empacotamentoapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User
+                .withUsername("admin")
+                .password("{noop}admin") // {noop} diz que não tem encoding
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                )
+                .httpBasic()
+                .and()
+                .build();
+    }
+}
