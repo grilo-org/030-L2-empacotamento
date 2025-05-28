d971d71679b1df8d40b3f2eae48c640eac11bea1
wesleymrosa <wesleymrosa@gmail.com>
2025-05-28 19:24:37 -0300

    Incluído o método empacotar no PedidoController. Rota POST para processar produtos e retornar as caixas

--- src/main/java/br/wesley/empacotamentoapi/controller/PedidoController.java
+++ src/main/java/br/wesley/empacotamentoapi/controller/PedidoController.java
@@ -1,20 +1,31 @@
 package br.wesley.empacotamentoapi.controller;
 
+import br.wesley.empacotamentoapi.dto.PedidoDTO;
+import br.wesley.empacotamentoapi.service.EmpacotamentoService;
+import br.wesley.empacotamentoapi.util.EmpacotadorDeProdutos;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/pedidos")
-@Tag(name = "Pedidos", description = "Endpoints relacionados ao empacotamento de pedidos")
+@Tag(name = "Pedidos", description = "API para empacotar pedidos em caixas")
 public class PedidoController {
 
-    @GetMapping("/teste")
-    @Operation(summary = "Endpoint de teste", description = "Retorna uma mensagem de confirmação de funcionamento do Swagger")
-    public ResponseEntity<String> testar() {
-        return ResponseEntity.ok("Swagger funcionando!");
+    @Autowired
+    private EmpacotamentoService empacotamentoService;
+
+    @PostMapping("/empacotar")
+    @Operation(summary = "Empacotar produtos de um pedido", description = "Retorna as caixas necessárias para empacotar os produtos enviados")
+    public ResponseEntity<List<EmpacotadorDeProdutos.Caixa>> empacotar(@RequestBody PedidoDTO pedidoDTO) {
+        List<EmpacotadorDeProdutos.Caixa> caixas = empacotamentoService.processarPedido(pedidoDTO);
+        return ResponseEntity.ok(caixas);
     }
 }
