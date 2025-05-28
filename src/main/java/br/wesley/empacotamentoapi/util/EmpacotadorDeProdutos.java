d825786a1e55bf480ac0c3d33f0b7c67dc3b952d
wesleymrosa <wesleymrosa@gmail.com>
2025-05-28 19:09:57 -0300

    Incluída a classe EmpacotadorDeProdutos no pacote util. Empacotamento de produtos baseado em volume de caixa

---
+++ src/main/java/br/wesley/empacotamentoapi/util/EmpacotadorDeProdutos.java
@@ -0,0 +1,73 @@
+package br.wesley.empacotamentoapi.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpacotadorDeProdutos {
+
+    private static final int COMPRIMENTO_CAIXA = 30;
+    private static final int LARGURA_CAIXA = 30;
+    private static final int ALTURA_CAIXA = 30;
+    private static final int VOLUME_CAIXA = COMPRIMENTO_CAIXA * LARGURA_CAIXA * ALTURA_CAIXA;
+
+    public static class Produto {
+        public String nome;
+        public int comprimento;
+        public int largura;
+        public int altura;
+
+        public Produto(String nome, int comprimento, int largura, int altura) {
+            this.nome = nome;
+            this.comprimento = comprimento;
+            this.largura = largura;
+            this.altura = altura;
+        }
+
+        public int getVolume() {
+            return comprimento * largura * altura;
+        }
+
+        @Override
+        public String toString() {
+            return nome + " (" + comprimento + "x" + largura + "x" + altura + ")";
+        }
+    }
+
+    public static class Caixa {
+        public List<Produto> produtos = new ArrayList<>();
+        public int volumeUsado = 0;
+
+        public boolean adicionarProduto(Produto produto) {
+            if (volumeUsado + produto.getVolume() <= VOLUME_CAIXA) {
+                produtos.add(produto);
+                volumeUsado += produto.getVolume();
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return "Caixa com " + produtos.size() + " produto(s): " + produtos;
+        }
+    }
+
+    public static List<Caixa> empacotar(List<Produto> produtos) {
+        List<Caixa> caixas = new ArrayList<>();
+        for (Produto produto : produtos) {
+            boolean empacotado = false;
+            for (Caixa caixa : caixas) {
+                if (caixa.adicionarProduto(produto)) {
+                    empacotado = true;
+                    break;
+                }
+            }
+            if (!empacotado) {
+                Caixa novaCaixa = new Caixa();
+                novaCaixa.adicionarProduto(produto);
+                caixas.add(novaCaixa);
+            }
+        }
+        return caixas;
+    }
+}
