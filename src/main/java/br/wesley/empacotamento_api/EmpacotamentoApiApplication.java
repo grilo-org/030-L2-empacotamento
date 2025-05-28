c0dfa1391118f8a3dae1dcc34b6a9c30f1dc5ee1
wesleymrosa <wesleymrosa@gmail.com>
2025-05-28 18:28:04 -0300

    Projeto base criado com o uso do initializr.

---
+++ src/main/java/br/wesley/empacotamento_api/EmpacotamentoApiApplication.java
@@ -0,0 +1,13 @@
+package br.wesley.empacotamento_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpacotamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpacotamentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/wesley/empacotamento_api/EmpacotamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.wesley.empacotamento_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpacotamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
